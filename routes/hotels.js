e21f1a2bac22df80a2991d71e30dd8f4b12cea0b
Hyunsoo Lee <wrkbrs@gmail.com>
2022-11-09 23:39:55 +0900

    routes 추가

---
+++ routes/hotels.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const router = express.Router();
+
+// CREATE
+router.post("/", (req, res) => {});
+// UPDATE
+// DELETE
+// GET
+// GET ALL
+export default router;
