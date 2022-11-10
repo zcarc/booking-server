9282b7c9bfdf91b487d427d7d7a468f21e2f4572
Hyunsoo Lee <wrkbrs@gmail.com>
2022-11-10 16:59:11 +0900

    hotel 컨트롤러 추가

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,81 +1,27 @@
 import express from "express";
-import Hotel from "../models/Hotel.js";
-import { createError } from "../utils/error.js";
+import {
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.js";
 
 const router = express.Router();
 
 // CREATE
-router.post("/", async (req, res) => {
-  console.log("post /hotels... req.body: ", req.body);
-
-  const newHotel = new Hotel(req.body);
-
-  console.log("newHotel: ", newHotel);
-
-  try {
-    const savedHotel = await newHotel.save();
-    res.status(200).json(savedHotel);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.post("/", createHotel);
 
 // UPDATE
-router.put("/:id", async (req, res) => {
-  console.log("put /:id... req.body: ", req.body);
-  console.log("req.params: ", req.params);
-
-  try {
-    const updatedHotel = await Hotel.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
-    console.log("updatedHotel: ", updatedHotel);
-    res.status(200).json(updatedHotel);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.put("/:id", updateHotel);
 
 // DELETE
-router.delete("/:id", async (req, res) => {
-  try {
-    await Hotel.findByIdAndDelete(req.params.id);
-    res.status(200).json("Hotel has been deleted");
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.delete("/:id", deleteHotel);
 
 // GET
-router.get("/:id", async (req, res) => {
-  try {
-    const hotel = await Hotel.findById(req.params.id);
-    res.status(200).json(hotel);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.get("/:id", getHotel);
 
 // GET ALL
-router.get("/", async (req, res, next) => {
-  console.log("hi im a hotel route");
-
-  const failed = true;
-
-  if (failed) {
-    return next(createError(401, "You are not authenticated!"));
-  }
-
-  try {
-    const hotels = await Hotel.findById("123123");
-    res.status(200).json(hotels);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/", getHotels);
 
 export default router;
