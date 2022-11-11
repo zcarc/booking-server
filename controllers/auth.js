8dfe87910c006754a983defcc7eb05b960096c0d
Hyunsoo Lee <wrkbrs@gmail.com>
2022-11-11 20:06:07 +0900

    auth (register, login) 컨트롤러 추가

---
+++ controllers/auth.js
@@ -0,0 +1,56 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { createError } from "../utils/error.js";
+
+export const register = (req, res, next) => {
+  try {
+    const saltRounds = 10;
+    const myPlaintextPassword = req.body.password;
+
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      bcrypt.hash(myPlaintextPassword, salt, async function (err, hash) {
+        const newUser = new User({
+          username: req.body.username,
+          email: req.body.email,
+          password: hash,
+        });
+
+        await newUser.save();
+
+        res.status(200).send("User has been created.");
+      });
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  console.log("req.body.username: ", req.body.username);
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    console.log("### user: ", user);
+
+    if (!user) {
+      return next(createError(404, "User not found!"));
+    }
+
+    const myPlaintextPassword = req.body.password;
+    const hash = user.password;
+
+    bcrypt.compare(myPlaintextPassword, hash, function (err, result) {
+      console.log("result: ", result);
+      if (!result) {
+        return next(createError(400, "Wrong password or username!"));
+      }
+
+      const { password, isAdmin, ...otherDetails } = user._doc;
+      console.log("...otherDetails: ", { ...otherDetails });
+
+      res.status(200).send({ ...otherDetails });
+    });
+  } catch (error) {
+    next(error);
+  }
+};
