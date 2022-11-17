9a48ca7db19e9b65d4bcdb370cd5c04213d7df7d
Hyunsoo Lee <wrkbrs@gmail.com>
2022-11-18 05:41:08 +0900

    JWT 시크릿 키 변수명 변경

--- controllers/auth.js
+++ controllers/auth.js
@@ -54,7 +54,7 @@ export const login = async (req, res, next) => {
 
         const token = jwt.sign(
           { id: user._id, isAdmin: user.isAdmin },
-          process.env.JWT
+          process.env.JWT_SECRET_KEY
         );
 
         res
