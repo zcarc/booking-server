7d213f99b5463277a9c85beafd0cfc9dc440a286
Hyunsoo Lee <wrkbrs@gmail.com>
2022-11-18 10:27:32 +0900

    room 컨트롤러 추가

---
+++ controllers/room.js
@@ -0,0 +1,78 @@
+import Hotel from "../models/Hotel.js";
+import Room from "../models/Room.js";
+
+export const createRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  const newRoom = new Room(req.body);
+
+  try {
+    // Room 저장
+    const savedRoom = await newRoom.save();
+
+    try {
+      // hotelid 파라미터로 Hotel을 찾고 저장된 Room의 id를 rooms 배열에 추가
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $push: { rooms: savedRoom._id },
+      });
+    } catch (err) {
+      next(err);
+    }
+
+    // 저장된 Room을 JSON으로 응답
+    res.status(200).json(savedRoom);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateRoom = async (req, res, next) => {
+  try {
+    const updatedRoom = await Room.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedRoom);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  try {
+    await Room.findByIdAndDelete(req.params.id);
+
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $pull: { rooms: req.params.id },
+      });
+    } catch (err) {
+      next(err);
+    }
+
+    res.status(200).json("Room has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getRoom = async (req, res, next) => {
+  try {
+    const room = await Room.findById(req.params.id);
+    res.status(200).json(room);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getRooms = async (req, res, next) => {
+  try {
+    const rooms = await Room.find();
+    res.status(200).json(rooms);
+  } catch (error) {
+    next(error);
+  }
+};
