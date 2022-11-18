16adb6d96359073e1c11a5f13bc37d2e5af8c2ca
Hyunsoo Lee <wrkbrs@gmail.com>
2022-11-18 11:21:13 +0900

    특정 city에 있는 hotel 개수 가져오는 API 루트 추가

--- routes/hotels.js
+++ routes/hotels.js
@@ -5,6 +5,7 @@ import {
   getHotel,
   getHotels,
   updateHotel,
+  countByCity,
 } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
@@ -20,9 +21,11 @@ router.put("/:id", verifyAdmin, updateHotel);
 router.delete("/:id", verifyAdmin, deleteHotel);
 
 // GET
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 
 // GET ALL
 router.get("/", getHotels);
+
+router.get("/countByCity", countByCity);
 
 export default router;
