b0b565799b765a75df175b1ed205edd0e62b7e72
Hyunsoo Lee <wrkbrs@gmail.com>
2022-11-20 16:34:39 +0900

    호텔의 모든 방을 가져오는 컨트롤러 추가

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -1,4 +1,5 @@
 import Hotel from "../models/Hotel.js";
+import Room from "../models/Room.js";
 
 export const createHotel = async (req, res, next) => {
   const newHotel = new Hotel(req.body);
@@ -97,6 +98,21 @@ export const countByType = async (req, res, next) => {
       { type: "빌라", count: villaCount },
       { type: "샬레", count: chaletCount },
     ]);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getHotelRooms = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    const list = await Promise.all(
+      hotel.rooms.map((room) => {
+        return Room.findById(room);
+      })
+    );
+
+    res.status(200).json(list);
   } catch (error) {
     next(error);
   }
